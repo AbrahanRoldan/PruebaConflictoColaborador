ea912ef77998b5e3c96e5da276d36e86a2791553
JaimeRT05 <romantejerojaime@gmail.com>
2024-10-23 09:56:12 +0200

    Correción conflicto bombisima

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,5 +1,5 @@
 public class HolaMundo{
     public static void main (String[] args){
-        System.out.println("¡Hola mundo en el login editandolo ahora te lo borrro yo ioepputtah!");
+        System.out.println("¡Hola mundo en el login editandolo ahora te lo borrro yo ioepputtah, tm!");
     }
 }
